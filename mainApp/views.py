c8fa62de45995c7c06b7cbb2b7a0524b3425d37d
solon <milimi974@gmail.com>
2018-02-19 19:50:57 +0100

    Supression de code de debug

--- mainApp/views.py
+++ mainApp/views.py
@@ -95,7 +95,6 @@ def get_request_results(url, params):
     """
     # send request and store result
     req = requests.get(url, params=params)
-    print(req.url)
     # convert result on json
     return req.json()
 
